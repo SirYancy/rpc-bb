c84164fabf08a8fc3d12f339c86418976c349eae
MH <yang5445@umn.edu>
2018-03-08 23:22:11 -0600

    Adding TCP functions

---
+++ tcp.c
@@ -0,0 +1,136 @@
+#include "tcp.h"
+
+bool InitCoordinator(int port) {
+}
+
+bool InitServer(int port) {
+    int serverSocket, clientSocket, c;
+    struct sockaddr_in server , client;
+     
+    //Create socket
+    serverSocket = socket(AF_INET , SOCK_STREAM , IPPROTO_TCP);
+    if (serverSocket == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+     
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(port);
+     
+    //Bind
+    if( bind(serverSocket,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        //print the error message
+        perror("bind failed. Error");
+        return 1;
+    }
+    puts("bind done");
+     
+    //Listen
+    listen(serverSocket , 3);
+     
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+     
+    while( (clientSocket = accept(serverSocket, (struct sockaddr *)&client, (socklen_t*)&c)) )
+    {
+        pthread_t clientThread;
+        int *newSocket = malloc(1);
+        *newSocket = clientSocket;
+
+        printf("New client connected\n");
+         
+        if(pthread_create(&clientThread, NULL, client_handler, (void *)newSocket) < 0) {
+            printf("Cannot create thread\n");
+            return false;
+        }
+         
+        pthread_join(clientThread, NULL);
+    }
+     
+    return true;
+}
+
+bool InitClient(char *serverIP, int serverPort, int serverSocket) {
+    struct sockaddr_in server;
+    char buffer[MAX_LEN];
+    int msgLen = 0;
+     
+    // Create socket for connecting to server
+    sendSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(sendSocket == -1) {
+        printf("Could not create socket\n");
+        return false;
+    }
+     
+    server.sin_addr.s_addr = inet_addr(serverIP);
+    server.sin_family = AF_INET;
+    server.sin_port = htons(serverPort);
+
+    //Bind
+    if(bind(recvSocket, (struct sockaddr *)&client, sizeof(client)) < 0) {
+        printf("Bind error\n");
+        return false;
+    }
+ 
+    //Connect to server
+    if(connect(sendSocket , (struct sockaddr *)&server , sizeof(server)) < 0) {
+        printf("Connection failed\n");
+        return false;
+    }
+    
+    return true;
+}
+
+void *client_handler(void *pSocket)
+{
+    int sock = *((int *)pSocket);
+    int recvSize;
+    char *message , client_message[2000];
+     
+     
+    message = "Now type something and i shall repeat what you type \n";
+    write(sock , message , strlen(message));
+     
+    //
+    while( (recvSize = recv(sock , client_message , 2000 , 0)) > 0 )
+    {
+        //Send the message back to client
+        write(sock , client_message , strlen(client_message));
+    }
+     
+    if(recvSize == 0) {
+        printf("Client disconnected\n");
+        fflush(stdout);
+    }else if(recvSize == -1) {
+        printf("Recv error");
+    }
+         
+    // Free socket pointer
+    free(pSocket);
+     
+    return;
+}
+
+bool SendThroughSocket(int socket, char *buffer, int len) {
+    int ret = 0;
+
+    ret = send(socket, buffer, sizeof(buffer), 0);
+    if(len == ret) {
+        return true;
+    } else if(ret == -1) {
+        printf("Send error\n");
+        return false;
+    } else {
+        // Size is not the same?
+        return false;
+    }
+}
+
+int RecvFromSocket(int socket, char *buffer) {
+    return recv(socket, buffer, MAX_LEN, 0);
+}
---
+++ tcp.h
@@ -0,0 +1,27 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define MAX_CONNECTION 10
+#define MAX_LEN 1024
+
+void *client_handler(void *);
+void *server_handler(void *);
+
+// Array of sockets for coordinator to store connected server sockets
+int aServerSocket[MAX_CONNECTION];
+
+// Array of sockets for server to store connected client sockets
+int aClientSocket[MAX_CONNECTION];
+
+bool InitCoordinator(int port);
+bool InitServer(int port);
+bool InitClient(char *serverIP, int serverPort, int serverSocket);
+
+bool SendThroughSocket(int socket, char *buffer, int len);
+int RecvFromSocket(int socket, char *buffer);
